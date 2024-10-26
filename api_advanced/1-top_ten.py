92711063ca80812de4bf731a69c52db5ce42fe8d
root <o.attah@alustudent.com>
2024-10-26 21:23:18 +0000

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,6 +2,9 @@
 """ top_ten.py """
 import requests
 
+
+
+
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
