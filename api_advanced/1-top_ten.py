3980ca992c40424f54692678ad9fd36c07204393
root <o.attah@alustudent.com>
2024-10-26 16:06:37 +0000

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-
-"""1-top_ten.py"""
-
 import requests
 
-
 def top_ten(subreddit):
-    """Top ten subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Prints the titles of the first 10 hot posts listed for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'My-User-Agent'}
-
+    
+    # Make a GET request to the Reddit API
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    # Check if the response status is 200 OK
     if response.status_code == 200:
         data = response.json()
-        posts = data.get('data').get('children')
-        [print(post.get('data').get('title')) for post in posts]
+        posts = data.get('data', {}).get('children', [])
+        
+        # Print titles of the first 10 posts
+        for post in posts:
+            print(post.get('data', {}).get('title'))
     else:
+        # Print None if the subreddit is invalid
         print(None)
+
