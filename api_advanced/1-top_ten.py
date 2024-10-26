0323a2dd3b29e57aaa17ba1c55733c4baf430fb1
root <o.attah@alustudent.com>
2024-10-26 20:51:43 +0000

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
+"""1-top_ten.py"""
+
+
 import requests
 
-def top_ten(subreddit):
-    """
-    Queries the Reddit API to print the titles of the first 10 hot posts for a given subreddit.
-    If the subreddit is invalid, prints None.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'My-User-Agent'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get("data", {}).get("children", [])
-            for post in posts:
-                print(post["data"]["title"])
-        else:
-            print(None)
-    except Exception:
-        print(None)
 
+def top_ten(subreddit):
+    """Top ten subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'My-User-Agent'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
+    else:
+        print(None)
